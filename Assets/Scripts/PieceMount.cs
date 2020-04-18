ad43db5ccaac27c34959f4fdb7713780d494fdcc
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-04-18 02:45:49 -0400

    Column added and working gameplay placing stuff

---
+++ Assets/Scripts/PieceMount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PieceMount : MonoBehaviour
+{
+    public int pieceID;
+
+    private bool pieceConnected = false;
+
+    void OnTriggerEnter(Collider thing)
+    {
+        var piece = thing.GetComponent<Piece>();
+        if (piece == null) return;
+        if (!pieceConnected && piece.pieceID == pieceID)
+        {
+            print("Hey that's the right piece");
+            pieceConnected = true;
+            piece.transform.SetParent(this.transform);
+            piece.transform.localPosition = Vector3.up;
+            piece.GetComponent<Rigidbody>().isKinematic = true;
+            piece.gameObject.layer = 0;
+        }
+    }
+}
